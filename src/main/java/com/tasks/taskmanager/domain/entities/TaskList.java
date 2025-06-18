d71e0f9ebe47a993d5578da8a939ce1b1254144b
thesauravpoddar <thesauravpoddar@gmail.com>
2025-06-18 20:01:42 +0530

    Initial commit

--- src/main/java/com/tasks/taskmanager/domain/entities/TaskList.java
+++ src/main/java/com/tasks/taskmanager/domain/entities/TaskList.java
@@ -28,4 +28,51 @@ public class TaskList {
     private LocalDateTime updated;
 
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskList taskList = (TaskList) o;
+        return Objects.equals(id, taskList.id) && Objects.equals(title, taskList.title) && Objects.equals(description, taskList.description) && Objects.equals(tasks, taskList.tasks) && Objects.equals(created, taskList.created) && Objects.equals(updated, taskList.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, tasks, created, updated);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", tasks=" + tasks +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
 }
